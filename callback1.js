73ce833d48f5fe889532274b0348974bf23dd78e
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-06-23 12:09:34 -0400

    2
    2

--- callback1.js
+++ callback1.js
@@ -7,3 +7,18 @@ setTimeout(f, 60*1000); // one minute
 console.log("I happen after setTimeout!");
 console.log("Me too!");
 
+code segment 2:
+
+function countdown() {
+   let i;                     // note we declare let outside of the for loop
+   console.log("Countdown:");
+   for(i=5; i>=0; i--) {
+      setTimeout(function() {
+         console.log(i===0 ? "GO!" : i);
+      }, (5-i)*1000);
+   }
+}
+countdown();
+
+
+
